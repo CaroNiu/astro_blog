591833368ed6b16d1c73c49f5edbc229f9aadca2
JMS\caro.niu <caro.niu@jtexpress.com>
2024-10-10 13:45:30 +0800

    应用初始化

---
+++ src/partials/Hero.tsx
@@ -0,0 +1,32 @@
+import {
+  GradientText,
+  HeroAvatar,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          Hi there, I'm <GradientText>Caro</GradientText> 👋
+        </>
+      }
+      description={
+        <>
+            Welcome to my blog space{' '}
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-64"
+          src="/assets/images/avatar.svg"
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+    />
+  </Section>
+);
+
+export { Hero };
